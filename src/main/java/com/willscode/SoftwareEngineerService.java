526b7d8dd0851aacb8b821b730cef8ff66cb08c5
Will C <34049597+Sn4keVenom@users.noreply.github.com>
2025-06-13 18:35:08 -0400

    Test

--- src/main/java/com/willscode/SoftwareEngineerService.java
+++ src/main/java/com/willscode/SoftwareEngineerService.java
@@ -13,7 +13,7 @@ public class SoftwareEngineerService {
         this.softwareEngineerRepository = softwareEngineerRepository;
     }
 
-    public List<SoftwareEngineer> getSoftwareEngineers(){
+    public List<SoftwareEngineer> getAllSoftwareEngineers(){
         return softwareEngineerRepository.findAll();
     }
 }
